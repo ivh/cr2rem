076718161576fa085b69c75b8550d72ef936edbd
marquart <marquart@318c8218-2dab-440d-9263-6f70bac63e5b>
2021-09-16 15:17:08 +0000

    manual
    git-svn-id: http://svnhq2.hq.eso.org/p2/trunk/Pipelines/cr2re/cr2rem@267056 318c8218-2dab-440d-9263-6f70bac63e5b

--- manual/man2tex.sh
+++ manual/man2tex.sh
@@ -4,6 +4,18 @@ FNAME=cr2res_manpages.tex
 
 echo "\section{Recipe man-pages}\label{sec:manpages}" > $FNAME
 
+cat <<EOT >> $FNAME
+The following is auto-generated pages with the manual-pages
+of each recipe. These can be viewed any time on the command-line
+with
+\begin{verbatim}
+    esorex --man-page <recipe>
+\end{verbatim}
+where \texttt{<recipe>} stands for the recipe name.
+
+EOT
+
+echo foo
 esorex --recipes | grep cr2re | cut -b3-23 | while read rec; do
     resc=$(echo $rec | sed s/_/\\\\_/g)
     printf "\subsection{${resc}}\n" >> $FNAME
